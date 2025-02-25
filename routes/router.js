d65adb04b1067afc6b06b623ce065662cc6ffdf5
lalit-shelkar <lalitshelkar2424@gmail.com>
2025-02-25 19:36:06 +0530

    initial commit

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { signupcontroller, verifyOtpController } = require('../controller/AuthController');
+const { signupcontroller, verifyOtpController, loginController } = require('../controller/AuthController');
 const router = express.Router();
 
 router.post("/signup", signupcontroller);
 router.post("/verifyotp", verifyOtpController);
-router.post("/login", verifyOtpController);
+router.post("/login", loginController);
 
 
 module.exports = router;
